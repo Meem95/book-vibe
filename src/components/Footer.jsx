ceeb4f5960806cbdb858753bc4b6891f7e9e8ebc
Meem Fatema <meemfatema95@gmail.com>
2024-03-26 22:49:03 -0400

    create footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 const Footer = () => {
   return (
     <div>
-      
+      <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+        <aside>
+          <p className="font-bold">Copyright © 2024 - All right reserved by Book Vibe</p>
+        </aside>
+      </footer>
     </div>
   );
 };
